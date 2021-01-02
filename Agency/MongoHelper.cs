47853e893d284b8227c66c9e5e1eb5d00ef87774
Aleksandra Tokareva <62117085+JanuaryAle@users.noreply.github.com>
2021-01-02 16:20:03 +0300

    Update MongoHelper.cs

--- Agency/MongoHelper.cs
+++ Agency/MongoHelper.cs
@@ -12,7 +12,7 @@ namespace Agency
         public static IMongoCollection<Models.Task> task_collection { get; set; }
         public static IMongoClient client { get; set; }
         public static IMongoDatabase database { get; set; }
-        public static string MongoConnection = "mongodb://crud_user:rb4gbDw7wMty72az@cluster-shard-00-00.3tjnw.mongodb.net:27017,cluster-shard-00-01.3tjnw.mongodb.net:27017,cluster-shard-00-02.3tjnw.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-9x4vfv-shard-0&authSource=admin&retryWrites=true&w=majority";
+        public static string MongoConnection = "mongodb://<user>:<password>@cluster-shard-00-00.3tjnw.mongodb.net:27017,cluster-shard-00-01.3tjnw.mongodb.net:27017,cluster-shard-00-02.3tjnw.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-9x4vfv-shard-0&authSource=admin&retryWrites=true&w=majority";
         public static string MongoDatabase = "Agency";
         internal static void ConnectToMongoService()
         {
